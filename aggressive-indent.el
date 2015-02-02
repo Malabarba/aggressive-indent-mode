3a252c8f9406260ab0422403d7050235eb0bc7fd
Artur Malabarba <bruce.connor.am@gmail.com>
2015-02-02 11:48:50 -0200

    Don't activate in text-mode or fundamental-mode

--- aggressive-indent.el
+++ aggressive-indent.el
@@ -370,6 +370,7 @@ strings."
   (if mode
       (if (and global-aggressive-indent-mode
                (or (cl-member-if #'derived-mode-p excluded-modes)
+                   (memq major-mode '(text-mode fundamental-mode))
                    buffer-read-only))
           (mode -1)
         ;; Should electric indent be ON or OFF?
