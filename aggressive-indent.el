746fb9e97a4ec477aee233c124be2b7f2161383d
Julian Knabenschuh <knabenschuh@doctronic.de>
2020-08-07 14:46:07 +0200

    Fixes missing timer error
    Fixes: Error running timer ‘aggressive-indent--indent-if-changed’: (wrong-type-argument timerp nil)

--- aggressive-indent.el
+++ aggressive-indent.el
@@ -462,7 +462,8 @@ If BODY finishes, `while-no-input' returns whatever value BODY produced."
 (defun aggressive-indent--indent-if-changed (buffer)
   "Indent any region that changed in BUFFER in the last command loop."
   (if (not (buffer-live-p buffer))
-      (cancel-timer aggressive-indent--idle-timer)
+      (when (timerp aggressive-indent--idle-timer)
+        (cancel-timer aggressive-indent--idle-timer))
     (with-current-buffer buffer
       (when (and aggressive-indent-mode aggressive-indent--changed-list)
         (save-excursion
